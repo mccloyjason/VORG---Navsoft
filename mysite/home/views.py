a05eec8b1aad79776407fb26f6e7974916f989ab
mccloyjason <mccloyjason@hotmail.com>
2014-09-19 18:23:08 +0530

    Login Page
    Login Page with html

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse 
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
+from passlib.hash import django_pbkdf2_sha256 as handler
 
 def index(request):
 	return render(request, 'home.html')
 def auth_view(request):
+	sessionlogin=request.session['login-session']
 	name = request.POST.get('Username', '')
 	pwd = request.POST.get('Password', '')
+	h=handler.encrypt(pwd,salt_size =12,rounds=12000)
 	details = User.objects.filter(username=name,password=pwd)
 	questions = [d.username for d in details]
-	return render(request, 'home.html', {'questions': questions})
+
+	request.session['login-session'] = questions
+	return render(request, 'home.html', {'questions': request.session['login-session'] })
